8e7f3d4f23c2f9101478e57c22ae6c2ea2e9bdbd
lizi <guoguanchen2000@mail.163.com>
2024-11-26 09:12:10 +0800

    generate disjoint and injective lean code in main

--- src/main.ml
+++ src/main.ml
@@ -10,10 +10,14 @@ let _ =
   let lexbuf = Lexing.from_channel (open_in Sys.argv.(1)) in
   let expr = Parser.main Lexer.token lexbuf in
   try
-    let verifyCode = genVerifyCode expr in
-    let vc = open_out "/home/code/temp.lean" in
-    Printf.fprintf vc "%s\n" verifyCode;
-    close_out vc;
+    let disjointCode = genDisjointCode expr in
+    let vc1 = open_out "/home/code/disjoint.lean" in
+    Printf.fprintf vc1 "%s\n" disjointCode;
+    close_out vc1;
+    let injectiveCode = genInjectiveCode expr in
+    let vc2 = open_out "/home/code/injective.lean" in
+    Printf.fprintf vc2 "%s\n" injectiveCode;
+    close_out vc2;
     let code = genCode expr in
     let _ = print_string "Generation finished\n" in
     let oc = open_out "/home/code/temp.dl" in
