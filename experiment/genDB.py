32fad4a56869011f92d0c823520f1008457d4cf4
lizi <guoguanchen2000@mail.163.com>
2025-10-24 09:50:58 +0800

    reorganize files

---
+++ experiment/genDB.py
@@ -0,0 +1,11 @@
+def genDBLuxuryitems(filePath, size):
+    pass
+
+def genDBAllitems(filePath, size):
+    pass
+
+def genDBCourses(filePath, size):
+    pass
+
+def genDBBookinfo(filePath, size):
+    pass
