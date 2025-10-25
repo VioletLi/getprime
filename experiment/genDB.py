2daf746ea8c69d22bac6f9bb8c5372c3f6147d99
lizi <guoguanchen2000@mail.163.com>
2025-10-25 21:01:11 +0800

    add data for efficiency

--- experiment/genDB.py
+++ experiment/genDB.py
@@ -1,7 +1,7 @@
 import random
 
 def genDBLuxuryitems(filePath, size):
-    datas=[]
+    datas = []
     for i in range(0, size//2):
         price = random.randint(0, 700)
         datas.append((i, price))
@@ -17,10 +17,45 @@ def genDBLuxuryitems(filePath, size):
 
 
 def genDBAllitems(filePath, size):
-    pass
+    with open(filePath,"w") as f:
+        for i in range(0,size//4):
+            f.write(f"on_sale('item_{i}').\n")
+        for i in range(size//4, size//2):
+            f.write(f"in_stock('item_{i}').\n")
+        for i in range(size//2,size//4*3):
+            f.write(f"on_sale('item_{i}').\nin_stock('item_{i}').\n")
+        for i in range(0,size//4*3):
+            f.write(f"all_items('item_{i}').\n")
+    print(f"Generated {size} items to {filePath}")
 
 def genDBCourses(filePath, size):
-    pass
+    with open(filePath, "w") as f:
+        for i in range(0, size // 10):
+            f.write(f"enrollment('course_{i}','stu_{i}').\n")
+        for i in range(size // 10, size * 2 // 10):
+            f.write(f"enrollment('course_{i}','stu_{i}1').\nenrollment('course_{i}','stu_{i}2').\n")
+        for i in range(size * 2 // 10, size * 3 // 10):
+            f.write(f"enrollment('course_{i}','stu_{i}1').\nenrollment('course_{i}','stu_{i}2').\nenrollment('course_{i}','stu_{i}3').\n")
+        for i in range(size * 3 // 10, size * 4 // 10):
+            f.write(f"enrollment('course_{i}','stu_{i}1').\nenrollment('course_{i}','stu_{i}2').\nenrollment('course_{i}','stu_{i}3').\nenrollment('course_{i}','stu_{i}4').\n")
+        for i in range(0, size * 4 // 10):
+            f.write(f"courses('course_{i}').\n")
+    print(f"Generated {size} items to {filePath}")
+
 
 def genDBBookinfo(filePath, size):
-    pass
+    group=size//6
+    with open(filePath, "w") as f:
+        for i in range(0, group+1):
+            f.write(f"author('book_{i}','author_{i}').\n")
+        for i in range(group+1,2*group+1):
+            f.write(f"author('book_{i}','author_{i}').\nversion('book_{i}','ver0').\n")
+        for i in range(2*group+1,3*group+2):
+            f.write(f"author('book_{i}','author_{i}').\nversion('book_{i}','ver0').\nversion('book_{i}','ver1').\n")
+        for i in range(0, group+1):
+            f.write(f"book_info('book_{i}','author_{i}','NULL').\n")
+        for i in range(group+1,2*group+1):
+            f.write(f"book_info('book_{i}','author_{i}','ver0').\n")
+        for i in range(2*group+1,3*group+2):
+            f.write(f"book_info('book_{i}','author_{i}','ver0').\nbook_info('book_{i}','author_{i}','ver1').\n")
+    print(f"Generated {size} items to {filePath}")
